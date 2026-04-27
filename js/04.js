abbd0928db9842f2f828a24d0fe0dc31d66544d8
Carlos181312 <crlsabm18@gmail.com>
2026-04-26 21:58:59 -0600

    Cambios Realizados

---
+++ js/04.js
@@ -0,0 +1,9 @@
+// String o Cadena de Texto
+
+const producto = "Monitor de 20\"";
+// const producto2 = String("Monitor 30 Pulgadas");
+// const producto3 = new String("Monitor 50 Pulgadas");
+
+console.log(producto)
+// console.log(typeof producto2)
+// console.log(typeof producto3)
