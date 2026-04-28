6503301bba73b7fa73e5eb7f0c66724f24d4b923
Carlos181312 <crlsabm18@gmail.com>
2026-04-27 18:37:04 -0600

    Cambios realizados

--- js/04.js
+++ js/04.js
@@ -1,9 +1,24 @@
 // String o Cadena de Texto
 
-const producto = "Monitor de 20\"";
+//const producto = "Monitor de 20\"";
+const tweet = "Aprendiendo JavaScript con el curso de Desarrollo Web Completo";
+const producto2 = "Monitor HD";
 // const producto2 = String("Monitor 30 Pulgadas");
 // const producto3 = new String("Monitor 50 Pulgadas");
 
-console.log(producto)
+
+// Length es para saber la extensión de caracteres
+console.log(tweet.length)
 // console.log(typeof producto2)
 // console.log(typeof producto3)
+console.log(producto2)
+
+
+// IndexOf (retorna posición)
+console.log(tweet.indexOf("JavaScript"));
+console.log(producto2.indexOf("Tablet"));
+
+// Includes (retorna True o False)
+console.log(tweet.includes("JavaScript"));
+console.log(producto2.includes("Tablet"));
+
---
+++ js/08.js
@@ -0,0 +1,11 @@
+const nombre = "Carlos";
+const email = "correo@correo.com";
+
+// Concatenación
+
+console.log("Nombre Cliente:" + " " + nombre + " Email: " + email);
+
+//Template Strings - String Literals
+
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`)
+
