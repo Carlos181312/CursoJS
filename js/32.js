0e86ecb349349ae43df0df16612d21532d206bb3
Carlos181312 <crlsabm18@gmail.com>
2026-05-04 11:01:10 -0600

    Finalización Curso

--- js/32.js
+++ js/32.js
@@ -11,11 +11,28 @@ function descargarNuevosClientes() {
     })
 }
 
+function descargarUltimosPedidos() {
+    return new Promise(resolve => {
+        console.log("Descargando pedidos... espere...");
+
+        setTimeout(function () {
+            resolve("Los pedidos fueron Descargados");
+        }, 3000)
+
+    })
+}
+
 
 async function app() {
     try {
-        const resultado = descargarNuevosClientes();
-        console.log(resultado)
+        // const clientes = await descargarNuevosClientes();
+        // const pedidos = await descargarUltimosPedidos();
+        // console.log(clientes)
+        // console.log(pedidos)
+
+        const resultado = await Promise.all([descargarNuevosClientes(), descargarUltimosPedidos()])
+        console.log(resultado[0])
+        console.log(resultado[1])
     } catch (error) {
         console.log(error)
     }
